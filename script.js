3656ca283d61ec72c6cf23f97a24f9e122ee1986
AbdullahBHC <abdullahbhc1@gmail.com>
2021-04-27 00:43:59 +0300

    Apple Calculator
    Yedıncı Web Projem

---
+++ script.js
@@ -0,0 +1,94 @@
+var sec2 = document.getElementById("section2");
+var sec6 = document.getElementById("section6");
+
+function cakesgoster() {
+    var sec1 = document.getElementById("section1");
+    var sec4 = document.getElementById("section4");
+    var sec5 = document.getElementById("section5");
+
+    if (sec1.style.display == "none" && sec4.style.display == "none" && sec5.style.display == "none") {
+        sec1.style.display = "block";
+        sec4.style.display = "block";
+        sec5.style.display = "block";
+    } else {
+        sec1.style.display = "none";
+        sec4.style.display = "none";
+        sec5.style.display = "none";
+    }
+}
+
+function cupcakesgoster() {
+    var sec1 = document.getElementById("section1");
+    var sec3 = document.getElementById("section3");
+    var sec4 = document.getElementById("section4");
+    var sec5 = document.getElementById("section5");
+
+    if (sec1.style.display == "none" && sec3.style.display == "none" && sec4.style.display == "none" && sec5.style.display == "none") {
+        sec1.style.display = "block";
+        sec3.style.display = "block";
+        sec4.style.display = "block";
+        sec5.style.display = "block";
+    } else {
+        sec1.style.display = "none";
+        sec3.style.display = "none";
+        sec4.style.display = "none";
+        sec5.style.display = "none";
+    }
+}
+
+function sweetsgoster() {
+    var sec2 = document.getElementById("section2");
+    var sec3 = document.getElementById("section3");
+    var sec4 = document.getElementById("section4");
+    var sec6 = document.getElementById("section6");
+
+    if (sec2.style.display == "none" && sec3.style.display == "none" && sec4.style.display == "none" && sec6.style.display == "none") {
+        sec2.style.display = "block";
+        sec3.style.display = "block";
+        sec4.style.display = "block";
+        sec6.style.display = "block";
+    } else {
+        sec2.style.display = "none";
+        sec3.style.display = "none";
+        sec4.style.display = "none";
+        sec6.style.display = "none";
+    }
+}
+
+function dougnutgoster() {
+    var sec1 = document.getElementById("section1");
+    var sec2 = document.getElementById("section2");
+    var sec3 = document.getElementById("section3");
+    var sec4 = document.getElementById("section4");
+    var sec6 = document.getElementById("section6");
+
+    if (sec1.style.display == "none" && sec2.style.display == "none" && sec3.style.display == "none" && sec4.style.display == "none" && sec6.style.display == "none") {
+        sec1.style.display = "block";
+        sec2.style.display = "block";
+        sec3.style.display = "block";
+        sec4.style.display = "block";
+        sec6.style.display = "block";
+    } else {
+        sec1.style.display = "none";
+        sec2.style.display = "none";
+        sec3.style.display = "none";
+        sec4.style.display = "none";
+        sec6.style.display = "none";
+    }
+}
+
+function tumunugoster() {
+    var sec1 = document.getElementById("section1");
+    var sec2 = document.getElementById("section2");
+    var sec3 = document.getElementById("section3");
+    var sec4 = document.getElementById("section4");
+    var sec5 = document.getElementById("section5");
+    var sec6 = document.getElementById("section6");
+
+    sec1.style.display = "block";
+    sec2.style.display = "block";
+    sec3.style.display = "block";
+    sec4.style.display = "block";
+    sec5.style.display = "block";
+    sec6.style.display = "block";
+}
